3360ae286bb02d01ab0916747c0bc92d3ddd25cc
Daniel Pink <dr.pink@gmail.com>
2017-05-19 09:37:10 +1000

    Initial blank XCode project

---
+++ NSTabViewControllerInSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTabViewControllerInSwift
+//
+//  Created by Daniel Pink on 19/5/17.
+//  Copyright © 2017 Daniel Pink. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTabViewControllerInSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTabViewControllerInSwift
+//
+//  Created by Daniel Pink on 19/5/17.
+//  Copyright © 2017 Daniel Pink. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
